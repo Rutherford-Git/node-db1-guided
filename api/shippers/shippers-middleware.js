fa4f776616401d5243e4c5aaa1b289ed5f62278a
Rutherford Alexis <rutherford.aalexis@Gmail.com>
2022-09-25 20:58:47 -0400

    followed along

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,25 @@
+const db = require('../../data/db-config')
+
 module.exports = {
   checkId,
   checkPayload,
 }
 
-function checkId(req, res, next) {
+async function checkId(req, res, next) {
+  const shipper = await db('shipper').where('shipperid', req.params.id).first()
+  if (!shipper){
+    next({
+      status: 404,
+      message: 'no id found'
+    })
+  }
   next()
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if (!req.body.Phone || !req.body.ShipperName){
+    next({ status: 422, message: 'Phone or name required'})
+  }else{ 
+    next()
+  }
 }
